b1e2dae12833e024447d58977c4caade82981923
aashish-ranjan <aashishranjan2011@gmail.com>
2023-02-01 04:32:42 +0530

    create dummy recycler view

--- app/src/main/java/com/example/notesdemo/MainActivity.kt
+++ app/src/main/java/com/example/notesdemo/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.notesdemo
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.notesdemo.models.Note
 
 class MainActivity : AppCompatActivity() {
+
+    //UI components
+    var recyclerView: RecyclerView? = null
+
+    //vars
+    var notesList: ArrayList<Note> = arrayListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initRecyclerView()
+        populateRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        recyclerView = findViewById(R.id.rv_notes_list)
+        recyclerView?.layoutManager = LinearLayoutManager(this)
+        recyclerView?.adapter = NotesRecyclerAdapter(notesList)
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun populateRecyclerView() {
+        notesList.addAll(
+            arrayListOf(
+                Note("Title a", timestamp = "01.01.2023"),
+                Note("Title b", timestamp = "02.01.2023"),
+                Note("Title c", timestamp = "03.01.2023")
+            )
+        )
+        recyclerView?.adapter?.notifyDataSetChanged()
     }
 }
---
+++ app/src/main/java/com/example/notesdemo/NotesRecyclerAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.notesdemo
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.notesdemo.models.Note
+
+class NotesRecyclerAdapter(private val notesList: ArrayList<Note>): RecyclerView.Adapter<NotesRecyclerAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.layout_note_item, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return notesList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val note = notesList[position]
+        holder.titleTextView.text = note.title
+        holder.timestampTextView.text = note.timestamp
+    }
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder)
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val titleTextView: TextView
+        val timestampTextView: TextView
+
+        init {
+            titleTextView = view.findViewById(R.id.tv_note_title)
+            timestampTextView = view.findViewById(R.id.tv_note_timestamp)
+        }
+    }
+}
