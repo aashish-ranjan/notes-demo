1885731e4852f90d7103d24d1715b64eb267ef9f
aashish-ranjan <aashishranjan2011@gmail.com>
2023-03-17 20:08:02 +0530

    implement right swipe delete gesture

--- app/src/main/java/com/example/notesdemo/NotesListActivity.kt
+++ app/src/main/java/com/example/notesdemo/NotesListActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.notesdemo.adapters.NotesRecyclerAdapter
@@ -15,10 +16,10 @@ import com.example.notesdemo.util.NotesRecyclerItemDecoration
 class NotesListActivity : AppCompatActivity(), NotesRecyclerAdapter.OnNoteItemClickListener {
 
     //UI components
-    var recyclerView: RecyclerView? = null
+    var mRecyclerView: RecyclerView? = null
 
     //vars
-    var notesList: ArrayList<Note> = arrayListOf()
+    var mNotesList: ArrayList<Note> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,31 +28,32 @@ class NotesListActivity : AppCompatActivity(), NotesRecyclerAdapter.OnNoteItemCl
         title = "Notes List"
         initRecyclerView()
         populateRecyclerView()
+        handleItemDeleteOnSwipeGesture()
     }
 
     private fun initRecyclerView() {
-        recyclerView = findViewById(R.id.rv_notes_list)
-        recyclerView?.also {
+        mRecyclerView = findViewById(R.id.rv_notes_list)
+        mRecyclerView?.also {
             it.layoutManager = LinearLayoutManager(this)
             it.addItemDecoration(
                 NotesRecyclerItemDecoration(10)
             )
-            it.adapter = NotesRecyclerAdapter(notesList, this)
+            it.adapter = NotesRecyclerAdapter(mNotesList, this)
         }
     }
 
     @SuppressLint("NotifyDataSetChanged")
     private fun populateRecyclerView() {
         repeat(100) { i ->
-            notesList.add(Note("Title #$i", content = "Content #$i", timestamp = "Timestamp #$i"))
+            mNotesList.add(Note("Title #$i", content = "Content #$i", timestamp = "Timestamp #$i"))
         }
-        recyclerView?.adapter?.notifyDataSetChanged()
+        mRecyclerView?.adapter?.notifyDataSetChanged()
     }
 
     override fun onNoteItemClicked(position: Int) {
         Log.d(TAG, "onNoteClicked at position $position")
         if (position > 0) {
-            val note = notesList[position]
+            val note = mNotesList[position]
             val intent = Intent(this, NotesDetailActivity::class.java)
             intent.putExtra("note_key", note)
             startActivity(intent)
@@ -61,6 +63,31 @@ class NotesListActivity : AppCompatActivity(), NotesRecyclerAdapter.OnNoteItemCl
     fun onAddNoteClicked(view: View) {
         val intent = Intent(this, NotesDetailActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun handleItemDeleteOnSwipeGesture() {
+        val itemTouchCallback = object: ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val noteIndex = viewHolder.absoluteAdapterPosition
+                deleteNote(mNotesList[noteIndex])
+            }
+        }
+        val itemTouchHelper = ItemTouchHelper(itemTouchCallback)
+        itemTouchHelper.attachToRecyclerView(mRecyclerView)
+    }
+
+    private fun deleteNote(note: Note) {
+        mNotesList.remove(note)
+        mRecyclerView?.adapter?.notifyDataSetChanged()
+
     }
 
     companion object {
